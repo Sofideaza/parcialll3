c6d79f8f8916f4594d069f0fd90530f8262b63ec
Sofia Deaza <sofiadeaza05@gmail.com>
2025-06-06 17:57:46 -0500

    incio

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0lzb_tHutHVTrltcrpiPlDXX0IooD-zY",
+  authDomain: "parcial-3-d958e.firebaseapp.com",
+  projectId: "parcial-3-d958e",
+  storageBucket: "parcial-3-d958e.firebasestorage.app",
+  messagingSenderId: "116042689138",
+  appId: "1:116042689138:web:1a1a0b5e004beaae7e70eb"
+};
+
+const app = initializeApp(firebaseConfig);
